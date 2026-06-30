369c8ae3df7b8f9c7a9667285f6db5deae52170b
Jeca <dijanic@gmail.com>
2026-06-30 16:29:16 +0200

    Added discounted products in filter

--- src/components/DiscountedProducts.vue
+++ src/components/DiscountedProducts.vue
@@ -1,9 +1,23 @@
 <template>\
-  <div>DiscountedProducts</div>
+  <h3>Discounted Products</h3>
+  <ul>
+    <li v-for="product in discountedProducts" :key="product.id" >
+      {{ product.name }} - ${{ product.price }} - {{ product.length }}
+    </li>
+  </ul>
 </template>
 
 <script>
+
+
+
 export default {
   name: 'DiscountedProducts',
+  inject: ['ProvidedProductList'],
+  computed: {
+    discountedProducts() {
+      return this.ProvidedProductList.filter(product => product.discount !== null)
+    }
+  }
 }
 </script>
