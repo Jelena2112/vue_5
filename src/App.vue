4901c5177f0479d7b1dfcf53ff2e16588d1583df
Jeca <dijanic@gmail.com>
2026-06-30 11:59:34 +0200

    Adding products component and props

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
+  <Products :ProductsList="ProductsList" />
 
 </template>
 
@@ -8,6 +9,35 @@
 
 export default {
   name: 'App',
+  data: () => ({
+    ProductsList: [
+      {
+        name: "HTML Course",
+        price: 150,
+        length: "2 months",
+      },
+      {
+        name: "CSS Course",
+        price: 180,
+        length: "3 months",
+      },
+      {
+        name: "JavaScript Course",
+        price: 250,
+        length: "5 months",
+      },
+      {
+        name: "React Course",
+        price: 350,
+        length: "4 months",
+      },
+      {
+        name: "Vue.js Course",
+        price: 300,
+        length: "2 months",
+      },
+    ],
+  })
 
 }
 </script>
---
+++ src/components/Products.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>Products</div>
+</template>
+
+<script>
+ export default {
+   name: 'Products',
+   props:{
+     ProductsList:Array
+   }
+ }
+</script>
