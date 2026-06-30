3e02684274e4521024da49b08c5e03e20138e198
Jeca <dijanic@gmail.com>
2026-06-30 11:44:36 +0200

    deleting unused components and props

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,14 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <Users msg="Welcome to Your Propppppp" :currentDate="currentDate"/>
+
 </template>
 
 <script>
-import Users from './components/Users.vue'
+
 
 export default {
   name: 'App',
-  data:() => ({
-    currentDate:'01/01/2025'
-  }),
-  components: {
-    Users
-  }
+
 }
 </script>
 
